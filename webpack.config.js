4e20a1b6b8497c657f0a0b89fd39990f43009fe6
Wescley Torres <jewescley@gmail.com>
2021-09-28 22:26:31 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,31 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: ['.js', '.ts', '.tsx', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          module: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }
+      ]
+    }
+    ]
   },
   devServer: {
     contentBase: './public',
