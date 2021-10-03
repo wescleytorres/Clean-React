a75a50a8f94fbf7372db518a83ff9dcc322b56fc
Wescley Torres <jewescley@gmail.com>
2021-10-03 18:52:57 -0300

    feat: add signup view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,3 +1,4 @@
+import { Signup } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ const Router: React.FC<Props> = ({ makeLogin }: Props) => (
   <BrowserRouter>
     <Switch>
       <Route path="/login" exact component={makeLogin} />
+      <Route path="/signup" exact component={Signup} />
     </Switch>
   </BrowserRouter>
 )
