23a97ffb2b8e0bd3d74cccc800d0256149bb25ce
Wescley Torres <jewescley@gmail.com>
2021-10-03 16:26:58 -0300

    fix: change LocalSaveAccessToken to correct file

--- src/main/factories/usecases/save-access-token/local-save-access-token-factory.ts
+++ src/main/factories/usecases/save-access-token/local-save-access-token-factory.ts
@@ -1,5 +1,5 @@
 import { LocalSaveAccessToken }
-  from '@/data/usecases/authentication/save-access-token/local-save-access-token'
+  from '@/data/usecases/save-access-token/local-save-access-token'
 import { SaveAccessToken } from '@/domain/usecases'
 import { makeLocalStorageAdapter }
   from '../../cache/local-storage-adapter-factory'
