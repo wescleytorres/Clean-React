26fc27a76b071f31d1e6888efe44c752f1e83282
Wescley Torres <jewescley@gmail.com>
2021-10-03 16:31:53 -0300

    feat: add AddCount usecase

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from '@/domain/models/account-model'
+
+export type AddAccountParams = {
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface AddAccount {
+  add: (params: AddAccountParams) => Promise<AccountModel>
+}
