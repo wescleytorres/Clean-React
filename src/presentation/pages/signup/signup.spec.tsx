986203530acccf19bc19d2714fbcde373853e61d
Wescley Torres <jewescley@gmail.com>
2021-10-03 20:54:40 -0300

    refactor: move duplicated code to helper file

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import faker from 'faker'
-import { cleanup, render, RenderResult, fireEvent } from '@testing-library/react'
+import { cleanup, render, RenderResult } from '@testing-library/react'
 import { Helper, ValidationStub } from '@/presentation/test'
 import SignUp from './signup'
 
@@ -23,14 +23,6 @@ const makeSut = (params?: SutParams): SutTypes => {
   }
 }
 
-const populateField = (
-  sut: RenderResult,
-  fieldName: string,
-  value = faker.random.word()): void => {
-  const input = sut.getByTestId(fieldName)
-  fireEvent.input(input, { target: { value } })
-}
-
 describe('Signup Component', () => {
   afterEach(cleanup)
 
@@ -48,7 +40,7 @@ describe('Signup Component', () => {
   test('Should show name error if Validation fails', () => {
     const validationError = faker.random.words()
     const { sut } = makeSut({ validationError })
-    populateField(sut, 'name')
+    Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name', validationError)
   })
 })
