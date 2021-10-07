ded1179ca1ce5ab7e9f53d5def027b18a524c024
Wescley Torres <jewescley@gmail.com>
2021-10-07 20:30:00 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,4 +1,5 @@
 import {
+  HttpGetParams,
   HttpPostClient,
   HttpPostParams,
   HttpResponse
@@ -17,5 +18,9 @@ export class AxiosHttpClient implements HttpPostClient {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
     }
+  }
+
+  async get (params: HttpGetParams): Promise<void> {
+    await axios.get(params.url)
   }
 }
